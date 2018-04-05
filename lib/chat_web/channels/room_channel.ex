7555af8fe342f8cb2f1ef6044df71b48c7d5c975
hassan shaikley <hassan.shaikley@gmail.com>
2018-04-05 00:32:22 -0600

    Messages backed up in db

--- lib/chat_web/channels/room_channel.ex
+++ lib/chat_web/channels/room_channel.ex
@@ -1,13 +1,14 @@
 defmodule ChatWeb.RoomChannel do
   use ChatWeb, :channel
 
-  def join("room:lobby", payload, socket) do
-    if authorized?(payload) do
-      {:ok, socket}
-    else
-      {:error, %{reason: "unauthorized"}}
-    end
+def join("room:lobby", payload, socket) do
+  if authorized?(payload) do
+    send(self(), :after_join)
+    {:ok, socket}
+  else
+    {:error, %{reason: "unauthorized"}}
   end
+end
 
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
@@ -18,13 +19,24 @@ defmodule ChatWeb.RoomChannel do
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (room:lobby).
   def handle_in("shout", payload, socket) do
+    Chat.Message.changeset(%Chat.Message{}, payload) |> Chat.Repo.insert  
+
     broadcast socket, "shout", payload
     {:noreply, socket}
   end
 
   def handle_in("connect", payload, socket) do
-    broadcast socket, "connect", %{"one" => :two, 3 => "four"}
+    broadcast socket, "connect", %{"one" => :two, 3 => %{"one" => :four}}
     {:noreply, socket}
+  end
+
+  def handle_info(:after_join, socket) do
+    Chat.Message.get_messages()
+    |> Enum.each(fn msg -> push(socket, "shout", %{
+        name: msg.name,
+        message: msg.message,
+      }) end)
+    {:noreply, socket} # :noreply
   end
 
 
