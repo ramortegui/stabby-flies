237f5bcf1fc486c894db8ab061228e7bf480a58a
hassan shaikley <hassan.shaikley@gmail.com>
2018-08-18 16:04:48 -0600

    Got started setting up ids on socket (broken)

--- lib/chat_web/channels/user_socket.ex
+++ lib/chat_web/channels/user_socket.ex
@@ -14,27 +14,39 @@ defmodule ChatWeb.UserSocket do
   # verification, you can put default assigns into
   # the socket that will be set for all channels, ie
   #
-  #     {:ok, assign(socket, :user_id, verified_user_id)}
+  # {:ok, assign(socket, :user_id, verified_user_id)}
   #
   # To deny connection, return `:error`.
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  def connect(_params, socket) do
-    {:ok, socket}
+
+  # def connect(params, socket) do
+  #   # {:ok, socket}
+  #   {:ok, assign(socket, :user_id, params["user_id"])}
+    
+  # end
+  def connect(%{"token" => token}, socket) do
+    # max_age: 1209600 is equivalent to two weeks in seconds
+    case Phoenix.Token.verify(socket, "user socket", token, max_age: 1209600) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user, user_id)}
+      {:error, reason} ->
+        :error
+    end
   end
 
   
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
   #
-  #     def id(socket), do: "user_socket:#{socket.assigns.user_id}"
+  def id(socket), do: "user_socket:#{socket.assigns.user_id}"
   #
   # Would allow you to broadcast a "disconnect" event and terminate
   # all active sockets and channels for a given user:
   #
-  #     ChatWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
+      # ChatWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
   #
   # Returning `nil` makes this socket anonymous.
-  def id(_socket), do: nil
+  # def id(_socket), do: nil
 end
--- lib/chat_web/router.ex
+++ lib/chat_web/router.ex
@@ -7,6 +7,8 @@ defmodule ChatWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+
+    plug :put_user_token
   end
 
   pipeline :api do
@@ -17,6 +19,15 @@ defmodule ChatWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+  end
+
+  defp put_user_token(conn, _) do
+    if current_user = conn.assigns[:current_user] do
+      token = Phoenix.Token.sign(conn, "user socket", current_user.id)
+      assign(conn, :user_token, token)
+    else
+      conn
+    end
   end
 
   # Other scopes may use custom stacks.
